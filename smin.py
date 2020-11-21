8331ee38e33da758e7767dc56247f9856ea313ab
Robson Pires <robsonvpires@gmail.com>
2020-11-20 22:56:18 -0300

    at 3

--- smin.py
+++ smin.py
@@ -1,5 +1,5 @@
 #lista de todos os fornecedores
-lista_fornecedores = [[]]
+lista_fornecedores = [['beto farias', 'tecnologia', 'betofarias@gmail.com', '990066551', '00011122233']]
 
 def cadastro_fornecedor():
 
@@ -11,19 +11,36 @@ def cadastro_fornecedor():
 
     fornecedor_nome = ""
     espaco = " "
+    ja_existe_nome = False
+
     fornecedor_area = ""
+
     fornecedor_email = ""
     arroba = "@"
     pontocom = ".com"
-    
+    ja_existe_email = False
+
     fornecedor_telefone = ""
+    ja_existe_telefone = False
+
     fornecedor_cpf = ""
+    ja_existe_cpf = False
 
     #verifica se o nome não esta em branco
-    while (len(fornecedor_nome) < 3) or (espaco not in fornecedor_nome):
+    while (len(fornecedor_nome) < 3) or (espaco not in fornecedor_nome) or (ja_existe_nome == True):
         fornecedor_nome = input("digite seu nome completo(nome e sobrenome): ")
         if (len(fornecedor_nome) < 3) or (espaco not in fornecedor_nome):
             print("digite nome e sobrenome separados por espaço")
+        
+        #verifica se o nome ja foi cadastrado
+        tamanho = len(lista_fornecedores)
+
+        for i in range(tamanho):
+            if (lista_fornecedores[i][0]) == fornecedor_nome:
+                print(f"{fornecedor_nome}, nome ja cadastrado")
+                ja_existe_nome = True
+            else:
+                ja_existe_nome = False
 
     #verifica se a area de atuação não esta em branco
     while len(fornecedor_area) < 2:
@@ -32,24 +49,53 @@ def cadastro_fornecedor():
             print("nome muito curto, tente denovo")
 
     #verifica se o email é valido
-    while (len(fornecedor_email) < 6) or (arroba not in fornecedor_email or pontocom not in fornecedor_email):
+    while (len(fornecedor_email) < 6) or (arroba not in fornecedor_email or pontocom not in fornecedor_email) or (ja_existe_email == True):
         
         fornecedor_email = input("digite o seu email: ")
         if (len(fornecedor_email) < 6) or (arroba not in fornecedor_email or pontocom not in fornecedor_email):
             print("email invalido, digite um email valido")
-                
+        
+        #verifica se o email ja foi cadastrado
+        tamanho = len(lista_fornecedores)
+
+        for i in range(tamanho):
+            if (lista_fornecedores[i][2]) == fornecedor_email:
+                print(f"{fornecedor_email}, email ja cadastrado")
+                ja_existe_email = True
+            else:
+                ja_existe_email = False  
 
     #verifica se o numero tem pelo menos 9 digitos, se não pede novamente
-    while len(fornecedor_telefone) < 9:
+    while (len(fornecedor_telefone) < 9) or (ja_existe_telefone == True):
         fornecedor_telefone = input("digite o seu telefone: ")
         if len(fornecedor_telefone) < 9:
             print("numero incompleto, pelo menos 9 digitos")
 
+        #verifica se o telefone ja foi cadastrado
+        tamanho = len(lista_fornecedores)
+
+        for i in range(tamanho):
+            if (lista_fornecedores[i][3]) == fornecedor_telefone:
+                print(f"{fornecedor_telefone}, telefone ja cadastrado")
+                ja_existe_telefone = True
+            else:
+                ja_existe_telefone  = False
+
     #verifica se o cpf tem o numero certo de digitos, se não pede cpf novamente
-    while len(fornecedor_cpf) != 11 :
+    while (len(fornecedor_cpf) != 11) or (ja_existe_cpf == True):
         fornecedor_cpf = input("digite o seu CPF/CNPJ: ")
         if len(fornecedor_cpf) != 11:
             print("cpf não valido")
+
+        #verifica se o cpf ja foi cadastrado
+        tamanho = len(lista_fornecedores)
+
+        for i in range(tamanho):
+            if (lista_fornecedores[i][4]) == fornecedor_cpf:
+                print(f"{fornecedor_cpf}, cpf ja cadastrado")
+                ja_existe_cpf = True
+            else:
+                ja_existe_cpf  = False
          
     #adiciona todos os itens na lista do fornecedor
     fornecedor.append(fornecedor_nome)
